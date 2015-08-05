338181c5beda6f6cc2df50ec67b8bbd92af3cafb
lexoyo <a.hoyau@silexlabs.org>
2015-08-06 00:08:09 +0200

    with mocha

--- src/main.js
+++ src/main.js
@@ -2,60 +2,83 @@ import WysiwygTester from './wysiwyg-tester';
 import selenium from 'selenium-standalone';
 import webdriverio from 'webdriverio';
 
-// env vars
-var SILEX_URL = process.env.SILEX_URL;
+class Main {
+  constructor() {
+    // env vars
+    this.silexUrl = process.env.SILEX_URL;
 
-if(!SILEX_URL) {
-  console.error('Env vars required: SILEX_URL');
-}
+    if(!this.silexUrl) {
+      console.error('Env vars required: SILEX_URL');
+    }
 
-console.log('Start browsing Silex at URL ', SILEX_URL);
-
-// start selenium
-selenium.install({
-  chrome: {
-    version: '2.15',
-    arch: process.arch,
-    baseURL: 'http://chromedriver.storage.googleapis.com'
+    console.log('Start browsing Silex at URL ', this.silexUrl);
   }
-}, () => selenium.start((err, child) => {
-  var options = {
-      desiredCapabilities: {
-          browserName: 'chrome'
+  getClient() {
+    return this.client;
+  }
+  initClient(cbk) {
+    // start selenium
+    selenium.install({
+      chrome: {
+        version: '2.15',
+        arch: process.arch,
+        baseURL: 'http://chromedriver.storage.googleapis.com'
       }
-  };
-  let client = webdriverio.remote(options);
-  client.init().url(SILEX_URL)
-    // wait for Silex UI to be displayed
-    .waitForVisible('.silex-menu', 10000, false)
-    .then((isVisible) => {
-      // wait for the blanck website to be loading
-      if (!isVisible) {
-        client = client.waitForVisible('.silex-stage.loading-website', 1000, false);
-      }
-      // wait for it to be loaded
-      client
-        .waitForVisible('.silex-stage.loading-website', 10000, true)
-        .then(() => {
-          // wait a little longer, so that the fade-in animation of the blank site is over
-          setTimeout(() => {
-            console.log('start test');
-
-            // test the wysiwyg
-            var wysiwygTester = new WysiwygTester()
-            wysiwygTester.testInsertText(client)
-              .then(wysiwygTester.testInsertContainer)
-              .then(() => {
-                client
-                  .saveScreenshot(__dirname + '/../after-test-wysiwyg.png')
-                  // stop webdriver and selenium
-                  .end()
-                    .then(() => {
-                      console.log('end tests');
-                      child.kill();
-                    });
+    }, () => selenium.start((err, child) => {
+      // store child for closing at the end
+      this.child = child;
+      // open a browser window
+      this.client = webdriverio.remote({
+        desiredCapabilities: {
+            browserName: 'chrome'
+        }
+      });
+      cbk();
+    }));
+  }
+  describeTests() {
+    var main = this;
+    // define init hook, exectuted by mocha before tests
+    before(function (done) {
+      this.timeout(10000);
+      main.initClient(function () {
+        // navigate to Silex
+        main.client.init().url(main.silexUrl)
+          // wait for Silex UI to be displayed
+          .waitForVisible('.silex-menu', 10000, false)
+          .then(function isVisible(e) {
+            // wait for the blanck website to be loading
+            if (!isVisible) {
+              main.client = main.client.waitForVisible('.silex-stage.loading-website', 1000, false);
+            }
+            // wait for it to be loaded
+            main.client
+              .waitForVisible('.silex-stage.loading-website', 10000, true)
+              .then(function () {
+                // wait a little longer, so that the fade-in animation of the blank site is over
+                setTimeout(function () {
+                  done();
+                }, 1000);
             });
-          }, 1000);
+          });
       });
     });
-}));
+    after(function (done) {
+      main.client.end().then(function () {
+        done();
+      });
+    });
+    // wysiwyg tests
+    describe('wysiwyg tests', () => {
+      var wysiwygTester = new WysiwygTester(this.getClient.bind(this));
+      wysiwygTester.describeTestText();
+      wysiwygTester.describeTestContainer();
+    });
+  }
+  runTests() {
+    mocha.run();
+  }
+}
+
+let main = new Main();
+main.describeTests();
--- src/wysiwyg-tester.js
+++ src/wysiwyg-tester.js
@@ -1,23 +1,30 @@
 import assert from 'assert';
+import {describe, it} from 'mocha';
 
 export default class WysiwygTester {
-  constructor() {
+  /**
+   * @param  {Main} getClient method which returns the main webdriver.io client's object
+   *                          the client object canot be passed directly since it is undefined when tests are described
+   * @constructor
+   */
+  constructor(getClient) {
+    this.getClient = getClient;
   }
-  testInsertText(client) {
-    return new Promise((resolve, reject) => {
-      client
+  describeTestText() {
+    it('insert a text box', (done) => {
+      this.getClient()
         .click('div=Insert')
         .click('div=Text box')
         .frame('silex-stage-iframe')
         .waitForVisible('div=New text box')
         .isVisible('div=New text box')
         .then((isVisible) => assert(isVisible))
-        .then(() => resolve(client));
+        .then(done);
     });
   }
-  testInsertContainer(client) {
-    return new Promise((resolve, reject) => {
-      resolve(client);
+  describeTestContainer() {
+    it('insert a container', (done) => {
+      done();
     });
   }
 }
